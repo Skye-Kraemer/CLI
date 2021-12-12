3a3a25bb01086f88211beedc06df09c58fe6673a
Skye-Kraemer <skye.kraemer@gmail.com>
2021-12-12 16:40:47 +1030

    Partial implementation of parseData function
    Obtains keys from data and stores in an ArrayList

--- CLI.java
+++ CLI.java
@@ -13,7 +13,8 @@ public class CLI {
 	* Accepts a CSV file and outputs the contents in YAML format to the console
 	*/
 	public static void main(String[] args) {
-		String data = readFile(args[0]);
+		ArrayList<String> data = readFile(args[0]);
+		parseData(data);
 	}
 	
 	
@@ -28,27 +29,56 @@ public class CLI {
 	/*
 	 * Separates the data in a comma separated string into a hashmap and returns the hashmap
 	 */
-	public static ArrayList<HashMap<String, String>> parseData() {
+	public static ArrayList<HashMap<String, String>> parseData(ArrayList<String> data) {
+		char c;
+		
+		ArrayList<String> keys = new ArrayList<String>();
+	
+		for (int i = 0; i < data.get(0).length(); i++) {
+			c =  data.get(0).charAt(i);
+			switch(c) {
+			case ',':
+				keys.add(word);
+				word = "";
+				break;
+			default:
+				word = word + c;
+			}
+		}
+		keys.add(word);
+		word = "";
+		printArray(keys);
+		
+		for (int i = 1; i < data.size(); i++) {
+			c =  data.get(i).charAt(i);
+			
+		}
+		
 		return null;
 	}
 	
+	//testing function
+	public static void printArray(ArrayList<String> array) {
+		for (int i = 0; i < array.size(); i++) {
+			System.out.print(array.get(i) + ", ");
+		}
+	}
 	
 	/*
-	 * Reads given file and returns data as a string
+	 * Reads given file and returns data as an array of Strings
 	 */
-	public static String readFile(String filename) {
+	public static ArrayList<String> readFile(String filename) {
+		
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(filename));
 			try {
-				StringBuilder stringBuilder = new StringBuilder();
+				ArrayList<String> data =  new ArrayList<String>();
 				String line = reader.readLine();
-
 				while (line != null) {
-					stringBuilder.append(line);
-					stringBuilder.append("\n");
+					data.add(line);
 					line = reader.readLine();
 				}
-				return stringBuilder.toString();
+				return data;
 			} 
 			finally {
 				reader.close();
@@ -57,6 +87,6 @@ public class CLI {
 		catch (IOException e) {
 			System.out.println("ERROR: Failed to read: " + filename);
 			return null;
-		}	
+		}
 	}
 }
