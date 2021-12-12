8929db775c067424c9d45955f7cd2666b754b255
Skye-Kraemer <skye.kraemer@gmail.com>
2021-12-12 16:10:33 +1030

    Initialization
    Setup and declaration of key functions

---
+++ CLI.java
@@ -0,0 +1,39 @@
+package CLI;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class test {
+	
+	
+	/*
+	* Accepts a CSV file and outputs the contents in YAML format to the console
+	*/
+	public static void main(String[] args) {
+		String data = readFile(args[0]);
+	}
+	
+	
+	/*
+	 * Prints data in YAML format
+	 */
+	public static void printData() {
+		
+	}
+	
+	
+	/*
+	 * Separates the data in a comma separated string into a hashmap and returns the hashmap
+	 */
+	public static ArrayList<HashMap<String, String>> parseData() {
+		return null;
+	}
+	
+	
+	/*
+	 * Reads given file and returns data as an array of Strings
+	 */
+	public static String readFile(String filename) {
+		return null;
+	}
+}
